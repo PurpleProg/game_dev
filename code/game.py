8b6cdfe4444d910f07478af5df12e0028b372b62
marcel_phippen <marcel.phippen@gmail.com>
2024-03-10 15:04:43 +0100

    pygame base

--- code/game.py
+++ code/game.py
@@ -1,11 +1,26 @@
-class Game:
-	def __init__():
-		pass
+import pygame
+from pygame.locals import *
+import sys
+import settings
 
-	def run():
+
+class Game:
+	def __init__(self):
+		self.playing = True
+		pygame.init()
+		self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+
+	def run(self):
 		# this is the main part of the code
 
 		# from there, we need to use a game state manager (or gsm) but idk how to do this properly
 		# for switching from Pause to playing or cinematiques
 
-		pass
+		# pygame mainloop
+		while self.playing:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					pygame.quit()
+					sys.exit()
+
+			pygame.display.update()
