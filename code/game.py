292b9cd013f2a0e322363fb7342585f6748c1e16
marcel_phippen <marcel.phippen@gmail.com>
2024-03-10 18:10:01 +0100

    named the windows + icon

--- code/game.py
+++ code/game.py
@@ -9,12 +9,14 @@ class Game:
 		self.playing = True
 		pygame.init()
 		self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+		pygame.display.set_caption("Game")
+		pygame.display.set_icon(pygame.surface.Surface((10, 10)))
 
 	def run(self):
 		# this is the main part of the code
 
 		# from there, we need to use a game state manager (or gsm) but idk how to do this properly
-		# for switching from Pause to playing or cinematiques
+		# for switching from Pause to playing or cinematic
 
 		# pygame mainloop
 		while self.playing:
