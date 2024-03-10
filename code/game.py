f5349c2f6eb62d200684d86f9f34d037a2bb91d1
marcel_phippen <marcel.phippen@gmail.com>
2024-03-10 14:21:40 +0100

    base structure

---
+++ code/game.py
@@ -0,0 +1,11 @@
+class Game:
+	def __init__():
+		pass
+
+	def run():
+		# this is the main part of the code
+
+		# from there, we need to use a game state manager (or gsm) but idk how to do this properly
+		# for switching from Pause to playing or cinematiques
+
+		pass
