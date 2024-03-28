3483175d7081a1e0003930091cac66a664d5a90c
Purple_Prog <marcel.phippen@proton.me>
2024-03-28 13:26:21 +0100

    rework acceleration

--- code/players/player.py
+++ code/players/player.py
@@ -12,10 +12,12 @@ class Player(pygame.sprite.Sprite):
         # movement vars
         self.position = pygame.math.Vector2(pos)
         self.speed = pygame.Vector2(settings.SPEED, settings.SPEED)
-        self.acceleration = pygame.math.Vector2(settings.SPEED, .01)
+        # kinematics vectors
+        self.acceleration = pygame.math.Vector2(0, 0)
+        self.ACCELERATION = pygame.math.Vector2(1, 1)
         self.direction = pygame.math.Vector2(0, 0)
         self.velocity = pygame.math.Vector2(0, 0)
-        self.friction = .94
+        self.friction = .20
 
         # default values are overwrite by each character
         self.image = pygame.Surface(size=(settings.TILE_SIZE, settings.TILE_SIZE))
@@ -39,7 +41,7 @@ class Player(pygame.sprite.Sprite):
 
         # scrolling
         self.position += self.camera.offset_float
-        self.rect.x, self.rect.y = int(self.position.x), int(self.position.y)
+        self.rect.x, self.rect.y = self.position.x, self.position.y
 
     def get_direction(self, pressed_keys: dict[str, bool]) -> None:
         if pressed_keys['UP']:
@@ -60,35 +62,32 @@ class Player(pygame.sprite.Sprite):
         if self.direction.length() > 1:
             self.direction = self.direction.normalize()
 
+        self.acceleration.x = self.direction.x * self.ACCELERATION.x
+        self.acceleration.y = self.direction.y * self.ACCELERATION.y
+
     def move_x(self, dt: float) -> None:
-        # self.velocity.x += self.speed.x * self.direction.x
-        self.velocity.x += self.acceleration.x * self.direction.x
-        self.velocity.x *= self.friction
-        print(self.velocity.x)
-        self.limit_x_vel()
-        # update pos and rect
-        self.position.x += self.velocity.x * dt
+        # update kinetics vectors
+        self.acceleration.x -= self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x
+
+        # limit velocity becoming 1.98754e-70
+        self.velocity.x = 0 if self.velocity.x < 0.01 and self.velocity.x > -0.01 else self.velocity.x
+
+        # change player position
+        self.position.x += self.velocity.x + .5*self.acceleration.x #  * dt
         self.rect.x = self.position.x
 
     def move_y(self, dt: float) -> None:
-        self.velocity.y += self.speed.y * self.direction.y + self.acceleration.y * self.direction.y
-        self.velocity.y *= self.friction
-        self.limit_y_vel()
-        # update rect and pos
-        self.position.y += self.velocity.y * dt
+        # update kinetics vectors
+        self.acceleration.y -= self.velocity.y * self.friction
+        self.velocity.y += self.acceleration.y
+
+        # limit velocity becoming 1.98754e-70
+        self.velocity.y = 0 if self.velocity.y < 0.01 and self.velocity.y > -0.01 else self.velocity.y
+
+        # change player position
+        self.position.y += self.velocity.y + .5*self.acceleration.y #  * dt
         self.rect.y = self.position.y
-
-    def limit_x_vel(self) -> None:
-        if abs(self.velocity.x) < 1 :
-            self.velocity.x = 0
-        if abs(self.velocity.x) > settings.MAX_VELOCITY:
-            self.velocity.x = settings.MAX_VELOCITY if self.velocity.x > 0 else -settings.MAX_VELOCITY
-
-    def limit_y_vel(self) -> None:
-        if abs(self.velocity.y) < 1:
-            self.velocity.y = 0
-        if abs(self.velocity.y) > settings.MAX_VELOCITY:
-            self.velocity.y = settings.MAX_VELOCITY if self.velocity.y > 0 else -settings.MAX_VELOCITY
 
     def collide_x(self):
         # player is from gameworld state, so here the stack is always gameworld.
--- code/settings.py
+++ code/settings.py
@@ -5,7 +5,6 @@ MIN_X, MAX_X = WIDTH*.1, WIDTH*.9
 MIN_Y, MAX_Y = HEIGHT*.1, HEIGHT*.9
 
 FPS = 120
-FIXED_FPS = 60  # NEVER change that value otherwise the framerate independence breaks
 TILE_SIZE = 16
 
 # players
